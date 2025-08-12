ffddad7fd30f18d04c9cf8a824d76df4b0d514fd
didou6661 <makhlouf.sanad@gmail.com>
2025-08-12 02:07:09 +0200

    add why us section

---
+++ src/components/WhyUsSection.js
@@ -0,0 +1,80 @@
+import React from "react";
+import "./WhyUsSection.css";
+import home from "../assets/home.png";
+import money from "../assets/money.png";
+import chart from "../assets/chart.png";
+import avatar from "../assets/Avatar.png";
+
+const WhyUsSection = () => {
+  return (
+    <section className="why-us-section">
+      <div className="why-us-container">
+        <div className="stats-container">
+          <div className="stats-group">
+            <div className="why-us-header">
+              <h2>
+                <span className="city-name">
+                  ABU DHABI'S <span className="colored-text">MOST <br /> </span>
+                </span> 
+                <span>TRUSTED </span>
+                <span className="colored-text">REAL ESTATE</span> PARTNER
+
+                
+              </h2>
+              <p className="why-us-description">
+                MG REAL ESTATE PARTNERS WITH ABU DHABI'S TOP DEVELOPERS AND
+                AGENCIES TO DELIVER 100% VERIFIED LISTINGS—SO YOU INVEST WITH
+                CONFIDENCE IN A MARKET BUILT ON TRANSPARENCY.
+              </p>
+            </div>
+
+            <div className="user-avatars">
+              {/* You'll need to add actual avatar images here */}
+             <img src={avatar} alt="avatar" />
+            </div>
+            <div className="stats-numbers">
+              <div className="stat">
+                <h3>10M</h3>
+                <p>Happy Buyers</p>
+              </div>
+              <div className="stat">
+                <h3>100K</h3>
+                <p>Client Review</p>
+              </div>
+              <div className="stat">
+                <h3>4.6</h3>
+                <p>Positive Rating</p>
+              </div>
+            </div>
+          </div>
+
+          <div className="features-list">
+            <div className="feature-item">
+              <img src={money} alt="money" />
+              <div className="feature-text">
+                <h4>Most Appreciated Projects</h4>
+                <p>Highest Capital Appreciation And Rental Income Projects.</p>
+              </div>
+            </div>
+            <div className="feature-item">
+              <img src={home} alt="home" />
+              <div className="feature-text">
+                <h4>Premium Rental Units</h4>
+                <p>Best Units Available In The Market</p>
+              </div>
+            </div>
+            <div className="feature-item">
+              <img src={chart} alt="chart" />
+              <div className="feature-text">
+                <h4>We Tailor Your Investment Plan</h4>
+                <p>Observe The Growth Of Your Capital.</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default WhyUsSection;
